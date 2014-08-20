sobanvuex/php-newrelic

commit 38698a8dc904ccd2c41246f4c0a777463c691340

Update NewRelic.php

src/NewRelic/NewRelic.php
@@ -96,7 +96,7 @@ class NewRelic
     {
         static $instance = null;
 
-        if (!$instance) {
+        if (!$instance === null) {
             $instance = new static();
         }
 

commit 39fa6f1d0de687b9932c285f8622853410fc043c

Fix typo with null check in getInstance.
The behavior was broken in 38698a8.  This should fix it and the test
should ensure it continues to work.

src/NewRelic/NewRelic.php
@@ -96,7 +96,7 @@ class NewRelic
     {
         static $instance = null;
 
-        if (!$instance === null) {
+        if ($instance === null) {
             $instance = new static();
         }
 

tests/NewRelic/NewRelicTest.php
@@ -38,6 +38,13 @@ class NewRelicTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\\NewRelic\\NewRelic', $newRelic);
     }
 
+    public function testGetInstanceReturnsSameInstance()
+    {
+        $newRelic = NewRelic::getInstance();
+        $newRelic2 = NewRelic::getInstance();
+        $this->assertSame($newRelic, $newRelic2);
+    }
+
     public function testCall()
     {
         $newRelic = new NewRelic;