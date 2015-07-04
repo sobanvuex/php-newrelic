ad668a7487818afe65c596b73d745a5054d5541d
Alex Soban <alex.soban@lightspeedpos.com>
2015-07-04 16:19:02 -0500

    Agent implementations for:
    - newrelic_add_custom_parameter
    - newrelic_add_custom_tracer
    - newrelic_background_job
    - newrelic_capture_params
    - newrelic_custom_metric
    - newrelic_disable_autorum

--- src/Agent.php
+++ src/Agent.php
@@ -23,11 +23,20 @@ class Agent
     protected $loaded;
 
     /**
+     * Common .ini settings
+     *
+     * @var array
+     */
+     protected $settings;
+
+    /**
      * Construct a new `Agent`
      */
-    public function __construct()
+    public function __construct(array $settings = [])
     {
         $this->loaded = extension_loaded('newrelic');
+        $this->settings = $settings;
+        $this->setSettings();
     }
 
     /**
@@ -38,5 +47,106 @@ class Agent
     public function isLoaded()
     {
         return $this->loaded;
+    }
+
+    /**
+     * Set .ini settings
+     *
+     * @link https://docs.newrelic.com/docs/agents/php-agent/configuration/php-agent-configuration
+     * @param array $settings Array of settings to apply
+     */
+    public function setSettings(array $settings = [])
+    {
+        $this->settings += $settings;
+
+        foreach ($this->settings as $setting => $value) {
+            if (ini_get($setting) != $value) {
+                ini_set($setting, $value);
+            }
+        }
+    }
+
+    /**
+     * Get .ini settings
+     *
+     * @param boolean $all
+     * @return array
+     */
+    public function getSettings($all = false, $details = false)
+    {
+        return $all ? ini_get_all('newrelic', $details) : $this->settings;
+    }
+
+    // PHP Agent API //
+
+    /**
+     * @link https://docs.newrelic.com/docs/agents/php-agent/configuration/php-agent-api#api-custom-param
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @return boolean
+     */
+    public function addCustomParameter($key, $value)
+    {
+        return $this->isLoaded() ? newrelic_add_custom_parameter($key, $value) : false;
+    }
+
+    /**
+     * @link https://docs.newrelic.com/docs/agents/php-agent/configuration/php-agent-api#api-custom-tracer
+     *
+     * @param string $callable
+     *
+     * @return boolean
+     */
+    public function addCustomTracer($callable)
+    {
+        return $this->isLoaded() ? newrelic_add_custom_tracer($callable) : false;
+    }
+
+    /**
+     * @link https://docs.newrelic.com/docs/agents/php-agent/configuration/php-agent-api#api-bg
+     *
+     * @param boolean $flag
+     */
+    public function backgroundJob($flag = true)
+    {
+        $this->isLoaded() && newrelic_background_job($flag);
+    }
+
+    /**
+     * @link https://docs.newrelic.com/docs/agents/php-agent/configuration/php-agent-api#api-capture-params
+     *
+     * @param boolean $enable
+     */
+    public function captureParams($enable = true)
+    {
+        $this->isLoaded() && newrelic_capture_params($enable);
+    }
+
+    /**
+     * @link https://docs.newrelic.com/docs/agents/php-agent/configuration/php-agent-api#api-custom-metric
+     *
+     * @param string $key
+     * @param float $value
+     *
+     * @return boolean
+     */
+    public function customMetric($key, $value)
+    {
+        return $this->isLoaded() ? newrelic_custom_metric($key, $value) : false;
+    }
+
+    /**
+     * @link https://docs.newrelic.com/docs/agents/php-agent/configuration/php-agent-api#api-rum-disable
+     * The return value can be different from `true` if the extension is not loaded
+     *
+     * @param boolean $return
+     *
+     * @return boolean
+     */
+    public function disableAutorum($return = true)
+    {
+        return $this->isLoaded() ? newrelic_disable_autorum() : $return;
     }
 }
